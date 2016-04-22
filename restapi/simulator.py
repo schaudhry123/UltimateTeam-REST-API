8c474f14376ff595d8a8d90ae8b5fc98982a4699
Arnav Mishra <arnavmishra@eduroamprvnat-172-21-98-120.near.illinois.edu>
2016-04-22 14:57:03 -0500

    modified simulator

--- restapi/simulator.py
+++ restapi/simulator.py
@@ -14,17 +14,17 @@ def compare_teams(team1, team2):
         if(player.position != 'Keeper'):
             conversion = float(player.goals)/max(1, float(player.appearances))
             spg = float(player.shots_per_game)
-            team1_chance[player.name] = conversion/spg * random.uniform(0.5, 1.5)
+            team1_chance[player.name] = conversion/spg * random.uniform(1, 1.5)
         else:
-            team1_keep = 10.0 - random.uniform(0.5, 1.5) * float(player.rating)
+            team1_keep = 10.0 - random.uniform(0.5, 1) * float(player.rating)
             
     for player in team2_players:
         if(player.position != 'Keeper'):
             conversion = float(player.goals)/max(1, float(player.appearances))
             spg = float(player.shots_per_game)
-            team2_chance[player.name] = conversion/spg * random.uniform(0.5, 1.5)
+            team2_chance[player.name] = conversion/spg * random.uniform(1, 1.5)
         else:
-            team2_keep = 10.0 - random.uniform(0.5, 1.5) * float(player.rating)
+            team2_keep = 10.0 - random.uniform(0.5, 1) * float(player.rating)
  
     one_scorers = {}
     two_scorers = {}
